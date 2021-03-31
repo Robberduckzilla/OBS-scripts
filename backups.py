ecbc0b85ea6d73b9103f2edd1bc9db261e9febb9
Robberduckzilla <rob.m.drake@icloud.com>
2021-03-31 20:45:23 +0100

    pep8

--- backups.py
+++ backups.py
@@ -4,15 +4,15 @@ import shutil
 import time
 
 delay_in_minutes = 0.1
-source = "C:/Users/isabelle/appdata/roaming/obs-studio/"
-dest =  "C:/Users/isabelle/Creative Cloud Files/Initiative Design/Client Work/02_Bristol Adventurers Guild/projects/01 Twitch Charity Stream/obs-backups/"
+source = 'C:/Users/isabelle/appdata/roaming/obs-studio/'
+dest =  'C:/Users/isabelle/Creative Cloud Files/Initiative Design/Client Work/02_Bristol Adventurers Guild/projects/01 Twitch Charity Stream/obs-backups/'
 
 print(f'Backing up OBS stuff every {delay_in_minutes} minutes.')
 i=0
 
 while(True):
     timestamp = datetime.datetime.now().strftime(r'%Y-%m-%d_%H-%M')
-    directory = dest + timestamp + "/"
+    directory = dest + timestamp + '/'
     shutil.copytree(source, directory) 
     
     i+=1
