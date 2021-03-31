a8535ea17003019f89dd71a3dedf2834e7d493e8
Robberduckzilla <rob.m.drake@icloud.com>
2021-03-31 20:30:40 +0100

    Update backups.py

--- backups.py
+++ backups.py
@@ -12,15 +12,10 @@ i=0
 
 while(True):
     timestamp = datetime.datetime.now().strftime(r'%Y-%m-%d_%H-%M')
-    src_files = os.listdir(source)
     directory = dest + timestamp + "/"
     os.makedirs(os.path.dirname(directory), exist_ok=True)
-
-    for file_name in src_files:
-        full_file_name = os.path.join(source, file_name)
-        if os.path.isfile(full_file_name):
-            shutil.copytree(full_file_name, directory + file_name) 
+    shutil.copytree(full_file_name, directory + file_name) 
+    
     i+=1
     print(f'Backups so far: {i}')
-    
     time.sleep(60 * delay_in_minutes)
