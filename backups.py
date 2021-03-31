0b4e4fb00aa39edff66031954bdb61670256dd99
Isabelle <63975451+TheTabletopGirl@users.noreply.github.com>
2021-03-31 21:22:32 +0100

    added directory and some emoji comments.

--- backups.py
+++ backups.py
@@ -5,7 +5,7 @@ from shutil import ignore_patterns
 import time
 
 delay_in_minutes = 30
-source = 'C:/Users/isabelle/appdata/roaming/obs-studio/'
+source = 'C:/Users/isabelle/appdata/roaming/obs-studio/basic/'
 dest =  'C:/Users/isabelle/Creative Cloud Files/Initiative Design/Client Work/02_Bristol Adventurers Guild/projects/01 Twitch Charity Stream/obs-backups/'
 
 print(f'Backing up OBS stuff every {delay_in_minutes} minutes.')
@@ -20,3 +20,6 @@ while(True):
     i+=1
     print(f'Backups so far: {i}')
     time.sleep(60 * delay_in_minutes)
+
+#👍
+#¯\_(ツ)_/¯
