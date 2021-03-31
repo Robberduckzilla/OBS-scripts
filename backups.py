0bf426c069da3796d9e78e4267d3d9cb35511b10
Robberduckzilla <rob.m.drake@icloud.com>
2021-03-31 20:48:18 +0100

    greg is lame

--- backups.py
+++ backups.py
@@ -3,11 +3,12 @@ import os
 import shutil
 import time
 
-delay_in_minutes = 0.1
+delay_in_minutes = 30
 source = 'C:/Users/isabelle/appdata/roaming/obs-studio/'
 dest =  'C:/Users/isabelle/Creative Cloud Files/Initiative Design/Client Work/02_Bristol Adventurers Guild/projects/01 Twitch Charity Stream/obs-backups/'
 
 print(f'Backing up OBS stuff every {delay_in_minutes} minutes.')
+
 i=0
 
 while(True):
