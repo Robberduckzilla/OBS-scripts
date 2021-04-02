91dfcb9a7cd73751ad788afbd6fb7c15e32cf336
Robberduckzilla <rob.m.drake@icloud.com>
2021-04-02 15:08:02 +0100

    fixed file_cycle and added stuff

--- text_file_cycle.py
+++ text_file_cycle.py
@@ -5,59 +5,51 @@ import obspython as obs
 from pathlib import Path
 from datetime import datetime
 import random
+import sys
 
-class CountdownToStream:
-    def __init__(self,source_name=None):
+seconds_between_lines = 10
+
+class TextCycler:
+    def __init__(self, source_name=None):
+        
+        self.lines = [x for x in open('C:/Users/robmd/Actual Local Files/GitHub/OBS-scripts/input.txt', 'r').readlines() if x != '']
+        random.shuffle(self.lines)
         self.source_name = source_name
-        self.lines = random.shuffle([x for x in open('C:/Users/robmd/Actual Local Files/GitHub/OBS-scripts/input.txt', 'r').readlines() if x !=''])
         self.line_index = 0
-        self.line_change_frequency_seconds = 1
         self.lastCount = ''
 
     def update_frequency(self, frequency_in_seconds):
         self.line_change_frequency_seconds = frequency_in_seconds
         
-    def update_text(self, force=False, updateTime=True):
-    """
-    ¯\_(ツ)_/¯
-    """
+    def update_text(self, force=False):
+        """
+        ¯\_(ツ)_/¯
+        """
         source = obs.obs_get_source_by_name(self.source_name)
         if source is not None:
-            if not Data._timerRunning_:
-                text_output = ''
-            else:
-                text_output = self.get_formatted_time()
+            if Data._timerRunning_:
+                text_output = self.choose_text_line()
 
             #prevent more work being done than necessary
             if(text_output == self.lastCount and not force):
                 return
-
             self.lastCount = text_output
+
             settings = obs.obs_data_create()
             obs.obs_data_set_string(settings, 'text', text_output)
             obs.obs_source_update(source, settings)
             obs.obs_data_release(settings)
             obs.obs_source_release(source)
 
-    def get_formatted_time(self):
+    def choose_text_line(self):
         text_output = Data._format_
-        
-        # calculate total seconds until the date
-        time_until_stream = int((datetime(2021, 4, 10, 12, 0, 0) - datetime.now()).total_seconds())
-        # prevent negative seconds
-        if time_until_stream <= 0:
-            time_until_stream = 0
-       
-        if time_until_stream % self.line_change_frequency_seconds == 0:            
-            self.line_index +=1
-            if self.line_index >= len(self.lines):
-                self.line_index = 0
 
-        next_line = self.lines[self.line_index]
-
+        self.line_index +=1
+        if self.line_index >= len(self.lines):
+            self.line_index = 0
+        next_line = self.lines[self.line_index].strip()        
         if '{text}' in Data._format_:
             text_output = str.replace(text_output, '{text}', f'{next_line}')
-
         return text_output
 
 class Data:
@@ -65,39 +57,34 @@ class Data:
     _format_ = _defaultFormat_
     _autoStart_ = True
     _timerRunning_ = True
+    _timeBetweenMessages_ = 1
 
-stream_countdown = CountdownToStream()
-callback = stream_countdown.update_text
+text_cycler = TextCycler()
+callback = text_cycler.update_text
 
 # ---------------------------- helper methods --------------------------------------
 
 def start_timer():
+    obs.timer_remove(callback)
+    obs.timer_add(callback, Data._timeBetweenMessages_ * 1000)
     Data._timerRunning_ = True
-    obs.timer_add(callback, 1 * 1000)
 
+def stop_timer():
+    Data._timerRunning_ = False
 # --------------------------- callbacks ---------------------------------------------
 
 def start_pressed(props, prop):
     start_timer()
 
-def on_event(event):
-    #if both autostart and autostop are diabled just return
-    if not Data._autoStart_: return
-
-    #stream start
-    if event == obs.OBS_FRONTEND_EVENT_STREAMING_STARTED and Data._autoStart_:
-        if stream_countdown.source_name != '':
-            start_timer()
-
 # -------------------------------------- script methods ----------------------------------------
 
 def script_update(settings):
-    stream_countdown.source_name = obs.obs_data_get_string(settings, 'source')
+    text_cycler.source_name = obs.obs_data_get_string(settings, 'source')
     Data._format_ = obs.obs_data_get_string(settings, 'format')
-    Data._autoStart_ = obs.obs_data_get_bool(settings, 'auto_start')
+    Data._timeBetweenMessages_ = obs.obs_data_get_int(settings, 'seconds_between_lines')
 
-    #force the text to update and do not increment the timer
-    stream_countdown.update_text(True, False)
+    # force the text to update and do not increment the timer
+    # text_cycler.update_text(True)
 
 def script_properties():
     props = obs.obs_properties_create()
@@ -120,18 +107,17 @@ def script_properties():
         obs.source_list_release(sources)
 
     obs.obs_properties_add_text(props, 'format', 'Text Format:', obs.OBS_TEXT_DEFAULT)
-    obs.obs_properties_add_button(props, 'start_button', 'Start Timer', start_pressed)
-    obs.obs_properties_add_bool(props, 'auto_start', 'Start Automatically with Stream')
+    obs.obs_properties_add_int_slider(props, 'seconds_between_lines', 'Seconds Between:', 1, 60, 1)
+    obs.obs_properties_add_button(props, 'start_button', 'Start Cycling', start_pressed)
 
     return props
 
 def script_load(settings):
-    obs.obs_frontend_add_event_callback(on_event)
     Data._format_ = obs.obs_data_get_string(settings, 'format')
-    Data._autoStart_ = obs.obs_data_get_bool(settings, 'auto_start')
-
+    Data._timeBetweenMessages_ = obs.obs_data_get_int(settings, 'seconds_between_lines')
     if not Data._format_:
         Data._format_ = Data._defaultFormat_
 
     obs.obs_data_set_string(settings, 'format', Data._format_)
-    obs.obs_data_set_bool(settings, 'auto_start', Data._autoStart_)
+    obs.obs_data_set_int(settings, 'seconds_between_lines', Data._timeBetweenMessages_)
+    
