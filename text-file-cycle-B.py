9cc7b6b30304b02a57bcf87e0b48a2635bea80ed
Robberduckzilla <rob.m.drake@icloud.com>
2021-04-03 22:26:03 +0100

    utf8 encoding, now on B!

--- text-file-cycle-B.py
+++ text-file-cycle-B.py
@@ -14,7 +14,9 @@ class TextCycler:
         self.lines = [x for x in open(
             'C:/Users/Isabelle/iCloudDrive/01 Design/personal OBS graphics/OBS-scripts/input-B.txt',
 #            'C:/Users/robmd/Actual Local Files/GitHub/OBS-scripts/input-B.txt',
-            'r').readlines() if x != '']
+            'r',
+            encoding='utf-8'
+            ).readlines() if x != '']
         random.shuffle(self.lines)
         self.source_name = source_name
         self.line_index = 0
