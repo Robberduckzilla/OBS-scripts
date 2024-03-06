1d8b915082273d984172a33184671209abe1ffc2
Robberduckzilla <rob.m.drake@icloud.com>
2024-03-06 14:15:36 +0000

    auto-start text cycling and add 00:00 end time

--- text-cycle-B.py
+++ text-cycle-B.py
@@ -119,8 +119,8 @@ def script_properties():
     obs.obs_properties_add_int_slider(
         props, "seconds_between_lines", "Seconds Between:", 1, 60, 1
     )
-    obs.obs_properties_add_button(props, "start_button", "Start Cycling", start_pressed)
 
+    start_timer()
     return props
 
 
