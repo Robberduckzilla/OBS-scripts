e7e96d9d25a32fca7bceda5867f3d8a46ca9e49d
Robberduckzilla <rob.m.drake@icloud.com>
2024-03-06 14:16:41 +0000

    add auto0start to donation amount

--- donation-amount.py
+++ donation-amount.py
@@ -86,8 +86,7 @@ def script_properties():
 
         obs.source_list_release(sources)
 
-    obs.obs_properties_add_button(props, "start_button", "Start Stuff", start_pressed)
-
+    start_timer()
     return props
 
 
